05350ee2003bb1e41c0e63e65180bc31ae622485
Saimone2 <hav1802200474@gmail.com>
2023-08-11 12:42:51 +0300

    the logic has been added, namely changing the volume, playback speed, and a timer scale has been created that shows the length of the track

--- src/main/java/com/saimone/mp3player/controller/MainController.java
+++ src/main/java/com/saimone/mp3player/controller/MainController.java
@@ -26,24 +26,19 @@ public class MainController implements Initializable {
     private ProgressBar songProgressBar;
     @FXML
     private Slider volumeSlider;
-
     private Media media;
     private MediaPlayer mediaPlayer;
-
-    private File directory;
-    private File[] files;
     private ArrayList<File> songs;
     private int songNumber;
     private final int[] speeds = {25, 50, 75, 100, 125, 150, 175, 200};
     private Timer timer;
-    private TimerTask timerTask;
     private boolean isRunning;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         songs = new ArrayList<>();
-        directory = new File("src/main/resources/com/saimone/mp3player/music");
-        files = directory.listFiles();
+        File directory = new File("src/main/resources/com/saimone/mp3player/music");
+        File[] files = directory.listFiles();
 
         if(files != null && files.length != 0) {
             Collections.addAll(songs, files);
@@ -52,21 +47,41 @@ public class MainController implements Initializable {
         media = new Media(songs.get(songNumber).toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         songLabel.setText(songs.get(songNumber).getName());
+
+        for (int speed : speeds) {
+            speedBox.getItems().add(speed + "%");
+        }
+
+        speedBox.setOnAction(this::changeSpeed);
+
+        volumeSlider.valueProperty().addListener((observableValue, number, t1) -> mediaPlayer.setVolume(volumeSlider.getValue() * 0.01));
+
+        songProgressBar.setStyle("-fx-accent: #00c853;");
     }
 
     public void playMedia() {
+        beginTimer();
+        changeSpeed(null);
+        mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
         mediaPlayer.play();
     }
 
     public void pauseMedia() {
-        mediaPlayer.pause();
+        if(timer != null) {
+            cancelTimer();
+            mediaPlayer.pause();
+        }
     }
 
     public void resetMedia() {
+        songProgressBar.setProgress(0);
         mediaPlayer.seek(Duration.seconds(0));
     }
 
     public void previousMedia() {
+        if(isRunning) {
+            cancelTimer();
+        }
         songNumber--;
 
         if (songNumber < 0) {
@@ -82,6 +97,9 @@ public class MainController implements Initializable {
     }
 
     public void nextMedia() {
+        if(isRunning) {
+            cancelTimer();
+        }
         songNumber++;
 
         if (songNumber >= songs.size()) {
@@ -97,15 +115,36 @@ public class MainController implements Initializable {
     }
 
     public void changeSpeed(ActionEvent event) {
-
+        if(speedBox.getValue() == null) {
+            mediaPlayer.setRate(1);
+        } else {
+            String stringVal = speedBox.getValue().substring(0, speedBox.getValue().length() - 1);
+            mediaPlayer.setRate(Integer.parseInt(stringVal) * 0.01);
+        }
     }
 
     public void beginTimer() {
+        timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                isRunning = true;
+                double current = mediaPlayer.getCurrentTime().toSeconds();
+                double end = media.getDuration().toSeconds();
 
+                songProgressBar.setProgress(current / end);
+
+                if (current / end == 1) {
+                    cancelTimer();
+                }
+            }
+        };
+
+        timer.scheduleAtFixedRate(timerTask, 0, 1000);
     }
 
     public void cancelTimer() {
-
+        isRunning = false;
+        timer.cancel();
     }
 }
-
