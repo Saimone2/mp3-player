8b7f4f565cdc1e08d0cc7687e8adc06bba0f240f
Saimone2 <hav1802200474@gmail.com>
2023-08-11 09:56:59 +0300

    added scene, controller for buttons and sliders without implementation

---
+++ src/main/java/com/saimone/mp3player/controller/MainController.java
@@ -0,0 +1,47 @@
+package com.saimone.mp3player.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void playMedia() {
+
+    }
+
+    public void pauseMedia() {
+
+    }
+
+    public void resetMedia() {
+
+    }
+
+    public void previousMedia() {
+
+    }
+
+    public void nextMedia() {
+
+    }
+
+    public void changeSpeed(ActionEvent event) {
+
+    }
+
+    public void beginTimer() {
+
+    }
+
+    public void cancelTimer() {
+
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.saimone.mp3player {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     opens com.saimone.mp3player to javafx.fxml;
     exports com.saimone.mp3player;
